18786f2a953fc9158d4900a9496a41c736dfd1c3
HoND09 <maximvelikorechin09@gmail.com>
2026-04-16 17:21:57 +0300

    final

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+};
+
+export const getPrimeQuestion = () => {
+  const number = getRandomInt(1, 100);
+
+  const question = number;
+  const answer = isPrime(number) ? 'yes' : 'no';
+
+  return { question, answer };
+};
+
+export const primeRule =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
